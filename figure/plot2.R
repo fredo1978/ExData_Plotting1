e319b5c5755c732028f325e6e8c1625a961af88f
Alfred Bangura <31698874+fredo1978@users.noreply.github.com>
2020-10-11 05:36:35 +0000

    Update plot2.R

--- figure/plot2.R
+++ figure/plot2.R
@@ -30,6 +30,8 @@ png(file = "Plot2.png", width = 480, height = 480, units = "px")
 # Generating Plot2
 
 plot(data$Global_active_power ~ data$DateTime, type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
 
 
 		 
